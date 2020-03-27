ec41ce36457cfcfacbe3f654a75d688ade429b85
Kimjeongguk <startli7949@gmail.com>
2020-03-27 17:58:08 +0900

    개인프로젝트

---
+++ Web_Project/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Project.Models
+{
+    public class Users
+    {
+        [Key] [Column(Order=1)]
+        public int UserNo { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string UserPassword { get; set; }
+        [Key] [Column(Order=2)]
+        public string UserNickname { get; set; }
+    }
+}
